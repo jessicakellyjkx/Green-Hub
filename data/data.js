e5987ea4189cf547e3bcda05c83d8b087fff6789
RyanFurt12 <ryanfurtadinho9@gmail.com>
2024-06-20 16:04:07 -0300

    feat: Add a json as a fake db

---
+++ data/data.js
@@ -0,0 +1,25 @@
+const database = 
+[
+    {
+        "titulo": "Título do Projeto",
+        "descricaoBasica": "Descrição básica do projeto.",
+        "descricaoDetalhada": "Descrição detalhada do projeto com mais informações.",
+        "tarefas": [
+            {
+                "titulo": "Título da Tarefa",
+                "imagem": "caminho/para/imagem.png",
+                "descricao": "Descrição detalhada da tarefa, se necessário.",
+                "contato": 
+                {
+                    "email": "email@example.com"
+                }
+            }
+        ],
+        "redesSociais": 
+        {
+            "facebook": "https://www.facebook.com/pagina",
+            "instagram": "https://www.instagram.com/pagina"
+        },
+        "linkMaisDetalhes": "index.html"
+    }
+]
